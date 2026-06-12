f9cb5216ea84775819a82a21ab17ce3613577560
carsonirvine <carsonspencerirvine@gmail.com>
2026-06-11 18:21:03 -0700

    Updated Files

--- project2.js
+++ project2.js
@@ -13,31 +13,31 @@ function analyze(data) {
 	
 	let region = "MISSING_REGION";
 	if(id < 152){
-		document.getElementById("region-image").src = "region1.jpg";
+		document.getElementById("region-image").src = "images/regions/region1.jpg";
 		region = "Kanto";
 	}else if(251 >= id){
-		document.getElementById("region-image").src = "region2.jpg";
+		document.getElementById("region-image").src = "images/regions/region2.jpg";
 		region = "Johto";
 	}else if(386 >= id){
-		document.getElementById("region-image").src = "region3.jpg";
+		document.getElementById("region-image").src = "images/regions/region3.jpg";
 		region = "Hoenn";
 	}else if(493 >= id){
-		document.getElementById("region-image").src = "region4.jpg";
+		document.getElementById("region-image").src = "images/regions/region4.jpg";
 		region = "Sinnoh";
 	}else if(649 >= id){
-		document.getElementById("region-image").src = "region5.jpg";
+		document.getElementById("region-image").src = "images/regions/region5.jpg";
 		region = "Unova";
 	}else if(721 >= id){
-		document.getElementById("region-image").src = "region6.jpg";
+		document.getElementById("region-image").src = "images/regions/region6.jpg";
 		region = "Kalos";
 	}else if(809 >= id){
-		document.getElementById("region-image").src = "region7.jpg";
+		document.getElementById("region-image").src = "images/regions/region7.jpg";
 		region = "Alola";
 	}else if(905 >= id){
-		document.getElementById("region-image").src = "region8.jpg";
+		document.getElementById("region-image").src = "images/regions/region8.jpg";
 		region = "Galar";
 	}else{
-		document.getElementById("region-image").src = "region9.jpg";
+		document.getElementById("region-image").src = "images/regions/region9.jpg";
 		region = "Hisui";
 	}
 	
